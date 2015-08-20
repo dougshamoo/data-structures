db5b99178a37d65cac8e36aa290f0a2f98efe61b
Doug Shamoo <doug.shamoo@gmail.com>
2015-08-20 16:06:50 -0700

    Functional-shared version of Queue passes all tests.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,41 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.count = 0;
+  someInstance.oldest = 0;
+  someInstance.newest = 0;
+  someInstance.storage = {};
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+
+    this.storage[this.newest] = value;
+    this.count ++;
+    this.newest ++;
+  },
+
+  dequeue: function(){
+    if (this.count > 0) {
+      this.count --;
+
+      var result = this.storage[this.oldest];
+      delete this.storage[this.oldest];
+      this.oldest ++;
+      if (this.count === 0) {
+        this.oldest = 0;
+        this.newest = 0;
+      }
+      return result;
+    }
+  },
+
+  size: function(){
+    return this.count;
+  }
+};
 
 
