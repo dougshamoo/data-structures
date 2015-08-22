2acb300e3d7fb45c430b80d82831dd4e39887dc8
Doug Shamoo <doug.shamoo@gmail.com>
2015-08-21 20:01:58 -0700

    Implemented HashTable, passes all tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,38 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(i) !== undefined) {
+    var newBucket = this._storage.get(i);
+    newBucket[k] = v;
+    this._storage.set(i, newBucket);
+  } else {
+    var bucket = {};
+    bucket[k] = v;
+    this._storage.set(i, bucket);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage.get(i);
+  for (var key in bucket) {
+    if (key === k) {
+      return bucket[key];
+    }
+  }
+  return null;
 };
 
-HashTable.prototype.remove = function(k){
+//[{'Steven':'Seagal'},,,,]
 
+HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  for (var key in bucket) {
+    if (key === k) {
+      delete bucket[key];
+    }
+  }
 };
 
 
