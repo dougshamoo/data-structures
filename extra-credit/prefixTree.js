f8cda1df05e056f20cf8ce3aea99d94fd0d3db91
Doug Shamoo <doug.shamoo@gmail.com>
2015-08-22 15:30:05 -0700

    Set up attempt at prefixTree

---
+++ extra-credit/prefixTree.js
@@ -0,0 +1,36 @@
+var prefixTree = function(value) {
+  var pt = {};
+  if (value === undefined) {
+    pt.letters = "";
+  }
+  pt.letters = value;
+
+}
+
+
+
+var prefixTreeMethods = {
+  getNum = function() {
+
+  }
+
+}
+
+
+
+// [null, null, [a,b,c], [d,e,f], [g,h,i],
+// [j,k,l], [m,n,o],[p,q,r,s], [t,u,v], [x,y,z]]
+
+
+// var random = prefixTree();
+
+// prefixTree.insert(1,2,3,4)
+
+// prefixTree.insert("Ball");
+
+
+// prefixTree.insert(2,2,8);
+
+
+
+
